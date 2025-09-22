317e525c7cb88bfa0724e986126016c16e9ca3b3
Dehyaneira Guevara <dehyaneira24@gmail.com>
2025-09-22 13:06:21 -0500

    Improve the user experience

--- src/index.js
+++ src/index.js
@@ -13,9 +13,12 @@ function generatePoem(event) {
   let apiKey = "0e93e42412aact309f35c9990ed8oba3";
   let prompt = `User instructions: Write a poem about ${instructionsInput.value} in Spanish with Ecuadorian words`;
   let context =
-    "You are an Ecuadorian romantic expert to write poems. Your mission is to generate a 4 line in basic HTML poem divide by <br/>. Do not put the word html. Higlight the word with <strong/> that the propmt says in the poem. Make sure to follow the user instructions.";
+    "You are an Ecuadorian romantic expert to write poems. Your mission is to generate a 4 line in basic HTML poem divide by <br/>. Do not put the word html. Make sure to follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating"> Generating a nice Ecuadorian Poem ⏳ </div>`;
   axios.get(apiUrl).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#instructions");
