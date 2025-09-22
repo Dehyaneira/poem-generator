c064de67af85226363e93fac43cb59abb9ca67c1
Dehyaneira Guevara <dehyaneira24@gmail.com>
2025-09-22 12:21:51 -0500

    added Axios and the API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
 function generatePoem(event) {
-    event.preventDefault();
-    
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "0e93e42412aact309f35c9990ed8oba3";
+  let prompt = `User instructions: Write a poem about ${instructionsInput.value} in Spanish with Ecuadorian words`;
+  let context =
+    "You are an Ecuadorian romantic expert to write poems. Your mission is to generate a 4 line in basic HTML poem divide by <br/>. Do not put the word html. Higlight the word with <strong/> that the propmt says in the poem. Make sure to follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    new Typewriter('#poem', {
-  strings: "Las rosas rojas son bonitas",
-  autoStart: true,
-  delay:1,
-  cursor: "",
-    });
+  axios.get(apiUrl).then(displayPoem);
 }
-let poemFormElement= document.querySelector("#instructions");
+let poemFormElement = document.querySelector("#instructions");
 poemFormElement.addEventListener("submit", generatePoem);
